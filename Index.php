871eb3c36a9041b298e86d5b80bcaf53db65764d
Tejo-TV <150041189+Tejo-TV@users.noreply.github.com>
2025-11-13 12:28:06 +0100

    Update: begin homepage
    begin gemaakt aan de homepage

--- Index.php
+++ Index.php
@@ -8,3 +8,21 @@
 //---------------------------------------------------------------------------------------------------//
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="shortcut icon" type="x-icon" href="assets/images/logo/small-logo.png">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <!-- Header -->
+    <header>
+            <a href="index.php"><img src="assets/images/logo/full-logo.png" alt="Logo"></a>
+            <input type="text" placeholder="Search..">
+    </header>
+    
+</body>
+</html>
